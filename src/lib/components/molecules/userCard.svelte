fe728621c498fa4ff3a06071ab51ef4df138a014
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-03 17:04:43 +0200

    feat: implement basic usercard

---
+++ src/lib/components/molecules/userCard.svelte
@@ -0,0 +1,40 @@
+<script lang="ts" context="module">
+	import { stakeholderStore } from '$store/stakeholder';
+</script>
+
+<script lang="ts">
+	export let name: string;
+	export let values: number[];
+	export let stakeholderIndex: number;
+</script>
+
+<div class="p-10 bg-yellow-100 shadow-lg flex flex-col">
+	<div class="font-bold text-2xl mb-4">{name}</div>
+	<table class="text-left">
+		<thead>
+			<tr>
+				<th>Schaal</th>
+				<th>Geholpen mensen</th>
+				<th>Kosten</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each values as v, valueIndex}
+				<tr>
+					<td
+						><input
+							type="range"
+							value={v}
+							max="10"
+							class="appearance-none w-full cursor-pointer accent-green-500 bg-black h-0.5"
+							on:input={(event) =>
+								($stakeholderStore[stakeholderIndex].values[valueIndex] = event.target?.value)}
+						/></td
+					>
+					<td>{v * 1000}</td>
+					<td>€{v * 50}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
---
+++ src/lib/store/stakeholder.ts
@@ -0,0 +1,15 @@
+import { writable } from 'svelte/store';
+
+interface Stakeholder {
+	name: string;
+	values: number[];
+}
+
+const stakeholders: Stakeholder[] = [
+	{ name: 'Gemeente', values: [4, 0, 0, 0] },
+	{ name: 'Vrijwilligers', values: [0, 0, 0, 0] },
+	{ name: 'Provincie', values: [0, 0, 0, 0] },
+	{ name: 'Studenten', values: [0, 0, 0, 0] }
+];
+
+export const stakeholderStore = writable<Stakeholder[]>(stakeholders);
