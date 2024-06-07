38367ef8ffcc0384ab73ff13dd7423f2ce79f126
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-07 23:27:56 +0200

    feat: add button components

---
+++ src/lib/components/atoms/buttons/ButtonMain.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" context="module">
+	import ButtonBase from './ButtonBase.svelte';
+	import { getClasses } from '$lib/utils/css';
+</script>
+
+<script lang="ts">
+	export { className as class };
+
+	export let theme: 'blue' | 'red' = 'blue';
+	export let href: string | undefined = undefined;
+	export let target: '_blank' | undefined = undefined;
+
+	let className = '';
+
+	$: classes = getClasses([
+		theme === 'blue' && 'bg-blue-500 text-white',
+		theme === 'red' && 'bg-red-500 text-white',
+		className
+	]);
+</script>
+
+<ButtonBase class="button-main px-5 rounded-md py-3 {classes}" {href} {target}>
+	<slot />
+</ButtonBase>
