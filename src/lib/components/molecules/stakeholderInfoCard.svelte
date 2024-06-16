97d93e0af6a8724f3af57b1dfb40cd4bbd4bf1bb
Verona Kragten <vkragten@sforsoftware.nl>
2024-06-16 16:34:58 +0200

    add description to interface and put in infocard (this does not work and it displays undifined 😔)

--- src/lib/components/molecules/stakeholderInfoCard.svelte
+++ src/lib/components/molecules/stakeholderInfoCard.svelte
@@ -10,11 +10,7 @@
 	<Card>
 		<h2 class="font-bold text-2xl">{$activeStakeholder.id}</h2>
 		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-			labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-			laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-			voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-			non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			{$activeStakeholder.description}
 		</p>
 
 		<div class="flex gap-x-2">
