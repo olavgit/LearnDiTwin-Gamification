b7719bc771f62359a768055e8d144eebc7877b12
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-17 10:49:02 +0200

    feat: add meta items

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,14 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>LearnDiTwin - BOB game</title>
+	<meta
+		name="description"
+		content="Ontdek de LearnDiTwin - BOB game: een innovatieve tool voor belanghebbenden om samen beslissingen te nemen. Door eerst gezamenlijk het probleem te begrijpen en mogelijke oplossingen te bedenken, vormt de BOB game een solide basis voor opinievorming en besluitvorming."
+	/>
+</svelte:head>
+
 <div class="main overflow-clip min-h-screen flex">
 	<slot />
 </div>
