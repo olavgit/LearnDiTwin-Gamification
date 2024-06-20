7f734e7c8889bc48189796fb659c880aa471bdca
Verona Kragten <vkragten@sforsoftware.nl>
2024-06-20 23:19:58 +0200

    add individual stakeholder result to resultcard

--- src/lib/components/molecules/resultCard.svelte
+++ src/lib/components/molecules/resultCard.svelte
@@ -39,25 +39,47 @@
 		calculateAverages();
 		calculateTotals();
 	}
-</script>
 
-<Card>
-	<div class="font-bold text-2xl mb-4">Resultaat</div>
-	{#each averages as average, index}
-		<div>
-			<span>{$maatregelenStore[index].naam.charAt(0).toUpperCase() + $maatregelenStore[index].naam.slice(1)} heeft een gemiddelde schaal van {average.toFixed(2)}</span>
-		</div>
-	{/each}
-	<div class="py-2">
-		<p>Geholpen mensen per jaar: {totalValue}</p>
-		<p>Kosten per jaar: {totalCost.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</p>
-		<p>Aantal vrijwilligers per jaar: {totalVrijwilligers}</p>
-	</div>
-	<div>
-		<ButtonMain
-			theme="red"
-			on:click={() => {
+	const stakeholders = [];
+	$stakeholderStore.forEach((stakeholder) => {
+		stakeholders.push(stakeholder)
+	});
+</script>
+<div class="flex flex-row">
+    <Card>
+        <div>
+            <div class="font-bold text-2xl mb-4">Resultaat</div>
+            {#each averages as average, index}
+                <div>
+                    <span>{$maatregelenStore[index].naam.charAt(0).toUpperCase() + $maatregelenStore[index].naam.slice(1)} heeft een gemiddelde schaal van {average.toFixed(2)}</span>
+                </div>
+            {/each}
+            <div class="py-2">
+                <p>Geholpen mensen per jaar: {totalValue}</p>
+                <p>Kosten per jaar: {totalCost.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR', minimumFractionDigits: 0, maximumFractionDigits: 0 })}</p>
+                <p>Aantal vrijwilligers per jaar: {totalVrijwilligers}</p>
+            </div>
+            <div>
+                <ButtonMain
+                        theme="red"
+                        on:click={() => {
 				appState.set(0);
-			}}>Terug</ButtonMain>	
-	</div>
-</Card>
+			}}>Terug</ButtonMain>
+            </div>
+        </div>
+    </Card>
+    	<div class="bg-white">
+    		{#each stakeholders as stakeholder}
+                <div  class="border-2 rounded m-2">
+    		<p class="border-y-2">
+                Team: <b>{stakeholder.id}</b> heeft het volgende gestemd:
+    		        </p>
+                        {#each stakeholder.values as value, index}
+                            <p>{$maatregelenStore[index].naam.charAt(0).toUpperCase() + $maatregelenStore[index].naam.slice(1)}: {value}
+                        {/each}
+                </div>
+                {/each}
+
+    	</div>
+</div>
+
