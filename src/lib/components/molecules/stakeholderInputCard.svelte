6189b4b378aeab65dc4e0ce7a65e7e983b953551
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-23 22:40:36 +0200

    refactor: cleanup

--- src/lib/components/molecules/stakeholderInputCard.svelte
+++ src/lib/components/molecules/stakeholderInputCard.svelte
@@ -4,9 +4,15 @@
 	import { onMount } from 'svelte';
 
 	import ButtonMain from '$atoms/buttons/ButtonMain.svelte';
+	import Card from '$atoms/Card.svelte';
 
 	import { appState } from '$store/app';
-	import { maatregelenStore, calculateCost, calculateValue, calculateVrijwilligers } from '$store/maatregelen';
+	import {
+		maatregelenStore,
+		calculateCost,
+		calculateValue,
+		calculateVrijwilligers
+	} from '$store/maatregelen';
 	import type { Maatregelen } from '$store/maatregelen';
 	import { doelenStore } from '$store/doelen';
 	import { stakeholderStore, activeStakeholder } from '$store/stakeholder';
@@ -14,17 +20,25 @@
 
 <script lang="ts">
 	onMount(() => {
-        const maatregelenCollection = collection(db, 'maatregelen');
-        const doelenCollection = collection(db, 'doelen');
+		const maatregelenCollection = collection(db, 'maatregelen');
+		const doelenCollection = collection(db, 'doelen');
 
-        onSnapshot(maatregelenCollection, (snapshot) => {
-            maatregelenStore.set(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })) as Maatregelen[]);
-        });
+		onSnapshot(maatregelenCollection, (snapshot) => {
+			maatregelenStore.set(
+				snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as Maatregelen[]
+			);
+		});
 
-        onSnapshot(doelenCollection, (snapshot) => {
-            doelenStore.set(snapshot.docs.map(doc => ({ doel: Number(doc.data().doel), budget: Number(doc.data().budget), ...doc.data()})));
-        });
-    });
+		onSnapshot(doelenCollection, (snapshot) => {
+			doelenStore.set(
+				snapshot.docs.map((doc) => ({
+					doel: Number(doc.data().doel),
+					budget: Number(doc.data().budget),
+					...doc.data()
+				}))
+			);
+		});
+	});
 
 	const onInput = (event: Event, index: number) => {
 		const target = event.target as HTMLInputElement;
@@ -55,63 +69,93 @@
 
 	$: active = $stakeholderStore.find((stakeholder) => stakeholder.id === $activeStakeholder?.id);
 
-	$: totalValue = $maatregelenStore.reduce((sum, maatregel, i) => sum + calculateValue(active?.values?.[i] ?? 0, maatregel), 0);
-	$: totalCost = $maatregelenStore.reduce((sum, maatregel, i) => sum + calculateCost(active?.values?.[i] ?? 0, maatregel), 0);
-	$: totalVrijwilligers = $maatregelenStore.reduce((sum, maatregel, i) => sum + calculateVrijwilligers(active?.values?.[i] ?? 0, maatregel), 0);
-	$: percentageDoel = totalValue / $doelenStore[0]?.doel * 100;
-	$: percentageBudget = totalCost / $doelenStore[0]?.budget * 100;
+	$: totalValue = $maatregelenStore.reduce(
+		(sum, maatregel, i) => sum + calculateValue(active?.values?.[i] ?? 0, maatregel),
+		0
+	);
+	$: totalCost = $maatregelenStore.reduce(
+		(sum, maatregel, i) => sum + calculateCost(active?.values?.[i] ?? 0, maatregel),
+		0
+	);
+	$: totalVrijwilligers = $maatregelenStore.reduce(
+		(sum, maatregel, i) => sum + calculateVrijwilligers(active?.values?.[i] ?? 0, maatregel),
+		0
+	);
+	$: percentageDoel = (totalValue / $doelenStore[0]?.doel) * 100;
+	$: percentageBudget = (totalCost / $doelenStore[0]?.budget) * 100;
 </script>
 
 {#if active}
-	<div class="p-10 bg-yellow-100 shadow-lg flex flex-col">
-		<div class="font-bold text-2xl mb-4">{active.id}</div>
-		<table class="text-left">
+	<Card class="lg:w-4/5">
+		<div class="font-bold text-2xl mb-4">Team {active.id}</div>
+		<table class="text-left mb-2">
 			<thead>
 				<tr>
-					<th class="px-2">Naam</th>
-					<th class="px-2">Schaal</th>
-					<th class="px-2">Geholpen mensen</th>
-					<th class="px-2">Kosten</th>
-					<th class="px-2">Vrijwilligers</th>
+					<th class="pr-4">Maatregel</th>
+					<th class="pr-4">Schaal</th>
+					<th class="pr-4">Geholpen mensen</th>
+					<th class="pr-4">Kosten</th>
+					<th>Vrijwilligers</th>
 				</tr>
 			</thead>
 			<tbody>
 				{#each $maatregelenStore as maatregel, i}
-				<tr>
-					<td class="px-2">{maatregel.naam.charAt(0).toUpperCase() + maatregel.naam.slice(1)}</td>
-					<td class="px-2">
-						<input
-							type="range"
-							value={active.values[i]}
-							min="0"
-							max="10"
-							class="slider accent-blue-500 align-middle appearance-none w-full cursor-pointer h-1 bg-red-500"
-							on:input={(e) => onInput(e, i)}
-						/>
-					</td>
-					<td class="px-2">{calculateValue(active.values[i], maatregel)}</td>
-					<td class="px-2">{calculateCost(active.values[i], maatregel).toLocaleString('nl-NL', { style: 'currency', currency: 'EUR',  minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
-					<td class="px-2">{calculateVrijwilligers(active.values[i], maatregel)}</td>
-				</tr>
+					<tr>
+						<td class="pr-4">{maatregel.naam.charAt(0).toUpperCase() + maatregel.naam.slice(1)}</td>
+						<td class="pr-4">
+							<input
+								type="range"
+								value={active.values[i]}
+								min="0"
+								max="10"
+								class="slider accent-blue-500 align-middle appearance-none w-full cursor-pointer h-1 bg-red-500"
+								on:input={(e) => onInput(e, i)}
+							/>
+						</td>
+						<td class="pr-4">{calculateValue(active.values[i], maatregel)}</td>
+						<td class="pr-4"
+							>{calculateCost(active.values[i], maatregel).toLocaleString('nl-NL', {
+								style: 'currency',
+								currency: 'EUR',
+								minimumFractionDigits: 0,
+								maximumFractionDigits: 0
+							})}</td
+						>
+						<td>{calculateVrijwilligers(active.values[i], maatregel)}</td>
+					</tr>
 				{/each}
 				<tr class="font-bold border-t-2 border-black">
-                    <td class="px-2 py-1">Totaal</td>
-                    <td class="px-2 py-1"></td>
-                    <td class="px-2 py-1">{totalValue}</td>
-                    <td class="px-2 py-1">{totalCost.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR',  minimumFractionDigits: 0, maximumFractionDigits: 0 })}</td>
-                    <td class="px-2 py-1">{totalVrijwilligers}</td>
-                </tr>
+					<td class="pr-4">Totaal</td>
+					<td class="pr-4"></td>
+					<td class="pr-4">{totalValue}</td>
+					<td class="pr-4"
+						>{totalCost.toLocaleString('nl-NL', {
+							style: 'currency',
+							currency: 'EUR',
+							minimumFractionDigits: 0,
+							maximumFractionDigits: 0
+						})}</td
+					>
+					<td>{totalVrijwilligers}</td>
+				</tr>
 				<tr>
-					<td class="px-2 py-1"></td>
-                    <td class="px-2 py-1"></td>
-                    <td class="px-2 py-1" class:bg-green-300={percentageDoel >= 100}>{percentageDoel.toFixed(0)}% van het doel</td>
-					<td class="px-2 py-1" class:bg-red-300={percentageBudget > 100}>{percentageBudget.toFixed(0)}% van het budget</td>
-					<td class="px-2 py-1"></td>
-
+					<td class="pr-4"></td>
+					<td class="pr-4"></td>
+					<td
+						class="pr-4"
+						class:bg-red-300={percentageDoel < 100}
+						class:bg-green-300={percentageDoel >= 100}>{percentageDoel.toFixed(0)}% van het doel</td
+					>
+					<td
+						class="pr-4"
+						class:bg-red-300={percentageBudget > 100}
+						class:bg-green-300={percentageBudget <= 100}
+						>{percentageBudget.toFixed(0)}% van het budget</td
+					>
+					<td></td>
 				</tr>
 			</tbody>
 		</table>
-
 		<div class="flex gap-x-2">
 			<ButtonMain
 				theme="red"
@@ -125,5 +169,5 @@
 				}}>Klaar</ButtonMain
 			>
 		</div>
-	</div>
+	</Card>
 {/if}
