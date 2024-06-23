1ac2f318f7ce532ac470ddc543e434b05784b097
yimei <oostromy@gmail.com>
2024-06-23 16:22:20 +0200

    Add logo

--- src/lib/components/molecules/startCard.svelte
+++ src/lib/components/molecules/startCard.svelte
@@ -3,52 +3,101 @@
 	import Card from '$atoms/Card.svelte';
 	import { db } from '../../firebase';
 	import { collection, getDocs, updateDoc } from 'firebase/firestore';
-
+	
 	import { appState } from '$store/app';
-	import logo from '/src/lib/assets/bob_game_logo.png'
-</script>
-
-<script lang='ts'>
-
+  </script>
+	
+  <script lang='ts'>
 	let settingsOpen = false;
-
+	
 	const resetFirestore = async () => {
-		const measuresCollection = collection(db, 'stakeholders');
-		const snapshot = await getDocs(measuresCollection);
-
-		let empty = [];
-		const valuesSize = snapshot.docs[0].data().values.length;
-		for (let i = 0; i < valuesSize; i++) empty.push(0);
-
-		console.log();
-		for (const stakeholderDoc of snapshot.docs) {
-			// why's my ide giving me an error here stfu
-			await updateDoc(stakeholderDoc.ref, {
-				values: empty
-			});
-		}
-		alert('Alle schalen staan weer op 0!');
+	  const measuresCollection = collection(db, 'stakeholders');
+	  const snapshot = await getDocs(measuresCollection);
+	
+	  let empty = [];
+	  const valuesSize = snapshot.docs[0].data().values.length;
+	  for (let i = 0; i < valuesSize; i++) empty.push(0);
+	
+	  for (const stakeholderDoc of snapshot.docs) {
+		await updateDoc(stakeholderDoc.ref, {
+		  values: empty
+		});
+	  }
+	  alert('Alle schalen staan weer op 0!');
 	};
-</script>
-
-<Card>
+  </script>
+	
+  <style>
+	Card {
+	  background-color: transparent !important; 
+	  box-shadow: none !important;
+	  border: none !important; 
+	}
+	
+	.logo-container {
+	  display: flex;
+	  justify-content: center;
+	  align-items: center;
+	  height: 160px; 
+	  margin-bottom: 10px; 
+	}
+	
+	main {
+	  border: none; 
+	}
+	
+	.logo-image {
+	  width: 200px; 
+	  height: auto; 
+	  border: none; 
+	}
+	
+	img {
+	  border: none; 
+	}
+	
+	.text-center {
+	  margin-bottom: 8px; 
+	}
+	
+	.button-container {
+	  margin-top: 20px;
+	  margin-bottom: -16px; 
+	}
+  
+	.welcome-text {
+	  font-weight: bold;
+	  margin-top: 20px; 
+	}
+  </style>
+	
+  <Card>
+	<div class="logo-container">
+	  <main>
+		<img src="/bob_logo.png" alt="BOB-game Logo" class="logo-image" />
+	  </main>
+	</div>
+	
 	{#if !settingsOpen}
-		<div class="lg:max-w-[400px] text-center" >
-
-			<p>
-				<img src={logo} alt="bob game logo" width="200"  class="mx-auto">
-
-				Welkom in de BOB-Game! Hier zul je op basis van je eigen aangewezen rol meedenken over een probleem en en samen met je medespelers tot een besluit komen over welke maatregelen genomen moeten worden.
-			</p>
-			<ButtonMain on:click={() => appState.set(1)}>Start</ButtonMain>
-			<ButtonMain on:click={() => (settingsOpen = !settingsOpen)}>Instellingen</ButtonMain>
+	  <div class="lg:max-w-[400px] text-center">
+		<p class="text-center welcome-text">
+		  Welkom bij de BOB-Game! <!-- Dikgedrukte welkomsttekst -->
+		</p>
+		<p class="text-center">
+		  Hier zul je op basis van je eigen aangewezen rol meedenken over een probleem en samen met je medespelers tot een besluit komen over welke maatregelen genomen moeten worden.
+		</p>
+		<div class="button-container">
+		  <ButtonMain on:click={() => appState.set(1)}>Start</ButtonMain>
+		  <ButtonMain on:click={() => (settingsOpen = !settingsOpen)}>Instellingen</ButtonMain>
 		</div>
+	  </div>
 	{/if}
 	{#if settingsOpen}
-		<div>
-			<ButtonMain on:click={resetFirestore}>Reset Schalen</ButtonMain>
-			<ButtonMain on:click={() => appState.set(5)}>Upload CSV</ButtonMain>
-			<ButtonMain theme='red' on:click={() => (settingsOpen = !settingsOpen)}>Terug</ButtonMain>
-		</div>
+	  <div class="button-container">
+		<ButtonMain on:click={resetFirestore}>Reset Schalen</ButtonMain>
+		<ButtonMain on:click={() => appState.set(5)}>Upload CSV</ButtonMain>
+		<ButtonMain theme='red' on:click={() => (settingsOpen = !settingsOpen)}>Terug</ButtonMain>
+	  </div>
 	{/if}
-</Card>
+  </Card>
+  
