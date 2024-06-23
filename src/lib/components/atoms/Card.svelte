69853643e7c5ca98ce53fe121784e51e9d10e2d3
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-23 20:42:34 +0200

    feat: add optional classes to card component

--- src/lib/components/atoms/Card.svelte
+++ src/lib/components/atoms/Card.svelte
@@ -1,3 +1,9 @@
-<div class="p-14 bg-white shadow-lg flex flex-col">
+<script lang="ts">
+	export { className as class };
+
+	let className = '';
+</script>
+
+<div class="p-14 bg-white shadow-lg flex flex-col {className}">
 	<slot />
 </div>
