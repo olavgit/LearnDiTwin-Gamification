4de124150f6046e7c3b224e4a7a641692a6cc712
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-23 20:42:23 +0200

    feat: add transitions and theme to button

--- src/lib/components/atoms/buttons/ButtonMain.svelte
+++ src/lib/components/atoms/buttons/ButtonMain.svelte
@@ -6,19 +6,25 @@
 <script lang="ts">
 	export { className as class };
 
-	export let theme: 'blue' | 'red' = 'blue';
+	export let theme: 'blue' | 'red' | 'green' = 'blue';
 	export let href: string | undefined = undefined;
 	export let target: '_blank' | undefined = undefined;
 
 	let className = '';
 
 	$: classes = getClasses([
-		theme === 'blue' && 'bg-blue-500 text-white',
-		theme === 'red' && 'bg-red-500 text-white',
+		theme === 'blue' && 'bg-blue-500 hover:bg-blue-600 text-white',
+		theme === 'red' && 'bg-red-500 hover:bg-red-600 text-white',
+		theme === 'green' && 'bg-green-500 hover:bg-green-600 text-white',
 		className
 	]);
 </script>
 
-<ButtonBase class="button-main px-5 rounded-md py-3 {classes}" {href} {target} on:click>
+<ButtonBase
+	class="button-main px-5 transition-colors duration-300 ease-in-out rounded-md py-3 {classes}"
+	{href}
+	{target}
+	on:click
+>
 	<slot />
 </ButtonBase>
