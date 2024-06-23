aead4c98f5a5ad2db7a87fd987832025ea609a93
Olav <95251237+olavgit@users.noreply.github.com>
2024-06-23 22:49:39 +0200

    feat: move results per team

--- src/lib/components/molecules/resultCard.svelte
+++ src/lib/components/molecules/resultCard.svelte
@@ -1,136 +1,123 @@
 <script lang="ts" context="module">
-    import ButtonMain from '$atoms/buttons/ButtonMain.svelte';
-    import Card from '$atoms/Card.svelte';
-    import { Tooltip, tooltip } from '@svelte-plugins/tooltips';
+	import ButtonMain from '$atoms/buttons/ButtonMain.svelte';
+	import Card from '$atoms/Card.svelte';
 
-    import {maatregelenStore, calculateCost, calculateValue, calculateVrijwilligers} from '$store/maatregelen';
-    import {appState} from '$store/app';
-    import {stakeholderStore} from '$store/stakeholder';
+	import {
+		maatregelenStore,
+		calculateCost,
+		calculateValue,
+		calculateVrijwilligers
+	} from '$store/maatregelen';
+	import { appState } from '$store/app';
+	import { stakeholderStore } from '$store/stakeholder';
 </script>
 
 <script lang="ts">
-    const sentimentMarge = 1;
-    let averages: number[] = [];
-    let totalValue = 0;
-    let totalCost = 0;
-    let totalVrijwilligers = 0;
-    let stakeholders = [];
+	import ResultOverview from './resultOverview.svelte';
+	let overviewVisible = false;
 
-    let autoHideTooltip = true;
+	let averages: number[] = [];
+	let totalValue = 0;
+	let totalCost = 0;
+	let totalVrijwilligers = 0;
+	let stakeholders = [];
 
-    setTimeout(() => {
-        autoHideTooltip = false;
-    }, 5000);
+	const calculateAverages = () => {
+		let sumArray: number[] = [];
+		let numStakeholders = $stakeholderStore.length;
 
-    const calculateAverages = () => {
-        let sumArray: number[] = [];
-        let numStakeholders = $stakeholderStore.length;
+		$stakeholderStore.forEach((stakeholder) => {
+			stakeholder.values.forEach((value, index) => {
+				if (!sumArray[index]) {
+					sumArray[index] = 0;
+				}
+				sumArray[index] += value;
+			});
+		});
 
-        $stakeholderStore.forEach((stakeholder) => {
-            stakeholder.values.forEach((value, index) => {
-                if (!sumArray[index]) {
-                    sumArray[index] = 0;
-                }
-                sumArray[index] += value;
-            });
-        });
+		averages = sumArray.map((sum) => sum / numStakeholders);
+	};
 
-        averages = sumArray.map((sum) => sum / numStakeholders);
-    };
+	const calculateTotals = () => {
+		totalValue = $maatregelenStore.reduce(
+			(sum, maatregel, i) => sum + calculateValue(averages[i] ?? 0, maatregel),
+			0
+		);
+		totalCost = $maatregelenStore.reduce(
+			(sum, maatregel, i) => sum + calculateCost(averages[i] ?? 0, maatregel),
+			0
+		);
+		totalVrijwilligers = $maatregelenStore.reduce(
+			(sum, maatregel, i) => sum + calculateVrijwilligers(averages[i] ?? 0, maatregel),
+			0
+		);
+	};
 
-    const calculateTotals = () => {
-        totalValue = $maatregelenStore.reduce((sum, maatregel, i) => sum + calculateValue(averages[i] ?? 0, maatregel), 0);
-        totalCost = $maatregelenStore.reduce((sum, maatregel, i) => sum + calculateCost(averages[i] ?? 0, maatregel), 0);
-        totalVrijwilligers = $maatregelenStore.reduce((sum, maatregel, i) => sum + calculateVrijwilligers(averages[i] ?? 0, maatregel), 0);
-    };
+	const getStakeholders = () => {
+		stakeholders = [];
+		$stakeholderStore.forEach((stakeholder) => {
+			stakeholders.push(stakeholder);
+		});
+	};
 
-    const getStakeholders = () => {
-        stakeholders = [];
-        $stakeholderStore.forEach((stakeholder) => {
-            stakeholders.push(stakeholder)
-        });
-
-    }
-
-    const calculateSentiment = (stakeholder) => {
-        const deviations = [];
-        for (let i = 0; i < averages.length; i++) {
-            const deviation =  averages[i] - stakeholder.values[i]
-            deviations.push(deviation)
-        }
-        const sumOfDeviations = deviations.reduce((partialSum, a) => partialSum + a, 0);
-        const averageDeviation = sumOfDeviations / deviations.length;
-        return averageDeviation;
-    }
-
-    $: if ($maatregelenStore.length && $stakeholderStore.length) {
-        calculateAverages();
-        calculateTotals();
-        getStakeholders();
-    }
-
-
+	$: if ($maatregelenStore.length && $stakeholderStore.length) {
+		calculateAverages();
+		calculateTotals();
+		getStakeholders();
+	}
 </script>
-<Card>
-    <div class="flex flex-row m">
-        <div class="border-2 rounded p-2">
-            <div class="font-bold text-2xl mb-4">Resultaat</div>
-            {#each averages as average, index}
-                <div>
-                    <span>{$maatregelenStore[index].naam.charAt(0).toUpperCase() + $maatregelenStore[index].naam.slice(1)}
-                        heeft een gemiddelde schaal van {average.toFixed(2)}</span>
-                </div>
-            {/each}
-            <div class="py-2">
-                <p>Geholpen mensen per jaar: {totalValue}</p>
-                <p>Kosten per jaar: {totalCost.toLocaleString('nl-NL', {
-                    style: 'currency',
-                    currency: 'EUR',
-                    minimumFractionDigits: 0,
-                    maximumFractionDigits: 0
-                })}</p>
-                <p>Aantal vrijwilligers per jaar: {totalVrijwilligers}</p>
-            </div>
-            <div>
-                <ButtonMain
-                        theme="red"
-                        on:click={() => {
-            appState.set(0);
-        }}>Terug
-                </ButtonMain>
-            </div>
-        </div>
-        <div class="grid grid-cols-2 justify-between">
-            {#each stakeholders as stakeholder}
-                <div class="border-2 rounded">
-                    <p class="border-y-2 px-2">
-                        Team <b>{stakeholder.id}</b> heeft het volgende gestemd:
-                    </p>
-                    {#if $maatregelenStore.length}
-                        {#each stakeholder.values as value, index}
-                            <p>{$maatregelenStore[index].naam.charAt(0).toUpperCase() + $maatregelenStore[index].naam.slice(1)}
-                                : {value}</p>
-                        {/each}
-                    {/if}
 
-                    <Tooltip
-                            content="In hoeverre de keuze van de vertegenwoordiger uitwijkt van het gemiddelde."
-                            action="click"
-                    ><u>Sentiment:</u></Tooltip
-                    > {#if calculateSentiment(stakeholder)> sentimentMarge || calculateSentiment(stakeholder) < -sentimentMarge}
-                    :(
-                    {:else if calculateSentiment(stakeholder) === sentimentMarge || calculateSentiment(stakeholder) === -sentimentMarge}
-                    :|
-                    {:else}
-                    :)
-                    {/if}
-                </div>
-            {/each}
-        </div>
-        <div class="example">
+<Card class="items-start">
+	<div class="font-bold text-2xl mb-4">Resultaat</div>
+	<table class="mb-4">
+		<tr>
+			<td class="font-bold pr-4">Maatregel</td>
 
-        </div>
-    </div>
+			<td class="font-bold">Schaal</td>
+		</tr>
+		{#each averages as average, i}
+			<tr>
+				<td class="pr-4">
+					Gemiddelde inzet {$maatregelenStore[i].naam}
+				</td>
+
+				<td>{average.toFixed(2)}</td>
+			</tr>
+		{/each}
+		<tr>
+			<td class="font-bold">Geholpen mensen per jaar</td>
+			<td>{totalValue}</td>
+		</tr>
+		<tr>
+			<td class="font-bold pr-4">Kosten per jaar</td>
+
+			<td>
+				{totalCost.toLocaleString('nl-NL', {
+					style: 'currency',
+					currency: 'EUR',
+					minimumFractionDigits: 0,
+					maximumFractionDigits: 0
+				})}
+			</td>
+		</tr>
+		<tr>
+			<td class="font-bold pr-4">Aantal vrijwilligers per jaar</td>
+			<td>{totalVrijwilligers}</td>
+		</tr>
+	</table>
+	<div class="flex gap-x-2 justify-center">
+		<ButtonMain
+			theme="red"
+			on:click={() => {
+				appState.set(0);
+			}}
+			>Terug
+		</ButtonMain>
+		<ButtonMain on:click={() => (overviewVisible = !overviewVisible)}
+			>Resultaten per team</ButtonMain
+		>
+	</div>
 </Card>
-
-
+{#if overviewVisible}
+	<ResultOverview {averages} />
+{/if}
---
+++ src/lib/components/molecules/resultOverview.svelte
@@ -0,0 +1,58 @@
+<script lang="ts" context="module">
+	import { Tooltip } from '@svelte-plugins/tooltips';
+
+	import Card from '$atoms/Card.svelte';
+
+	import { maatregelenStore } from '$store/maatregelen';
+	import { stakeholderStore } from '$store/stakeholder';
+</script>
+
+<script lang="ts">
+	export let averages: number[] = [];
+
+	const sentimentMarge = 1;
+
+	const calculateSentiment = (stakeholderVals: number[]) => {
+		console.log(stakeholderVals);
+		const deviations = [];
+		for (let i = 0; i < averages.length; i++) {
+			const deviation = averages[i] - stakeholderVals[i];
+			deviations.push(deviation);
+		}
+		const sumOfDeviations = deviations.reduce((partialSum, a) => partialSum + a, 0);
+		const averageDeviation = sumOfDeviations / deviations.length;
+		console.log(averageDeviation);
+		console.log(deviations);
+		return averageDeviation;
+	};
+</script>
+
+<Card>
+	<p class="font-bold text-xl mb-2">Resultaten per team</p>
+	<div class="grid grid-cols-2 gap-4">
+		{#each $stakeholderStore as stakeholder}
+			<div class="bg-yellow-50">
+				<p class="font-bold">
+					{stakeholder.id}
+				</p>
+				{#each stakeholder.values as value, index}
+					<p class="capitalize">
+						{$maatregelenStore[index].naam}: {value}
+					</p>
+				{/each}
+
+				<Tooltip
+					content="In hoeverre de keuze van de vertegenwoordiger uitwijkt van het gemiddelde."
+					action="click"><u>Sentiment:</u></Tooltip
+				>
+				{#if calculateSentiment(stakeholder.values) > sentimentMarge || calculateSentiment(stakeholder.values) < -sentimentMarge}
+					:(
+				{:else if calculateSentiment(stakeholder.values) === sentimentMarge || calculateSentiment(stakeholder.values) === -sentimentMarge}
+					:|
+				{:else}
+					:)
+				{/if}
+			</div>
+		{/each}
+	</div>
+</Card>
